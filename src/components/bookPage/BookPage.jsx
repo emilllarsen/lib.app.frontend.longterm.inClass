481df66cf38a6f4200ca98031299fb487b86f23c
Emil Larsen <emilvl@stud.ntnu.no>
2026-03-24 10:49:56 +0100

    componens with welcome, userpage and bookpage

---
+++ src/components/bookPage/BookPage.jsx
@@ -0,0 +1,5 @@
+export default function BookPage(){
+    return(
+        <h1>A List of Books!</h1>
+    );
+}
---
+++ src/components/userPage/UserPage.jsx
@@ -0,0 +1,5 @@
+export default function UserPage(){
+    return(
+        <h1>A List of <b>Users</b>!</h1>
+    );
+}
