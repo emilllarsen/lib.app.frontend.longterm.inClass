2ea371eff98982f3db7b0d8d13899bd9919949d1
Emil Larsen <emilvl@stud.ntnu.no>
2026-03-24 11:05:52 +0100

    comment

--- src/components/bookDetailPage/BookDetailPage.jsx
+++ src/components/bookDetailPage/BookDetailPage.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router";
 
 export default function BookDetailPage(){
-    const { bid } = useParams();
+    const { bid } = useParams(); // Hook
     return(
         <h1>A list of Books: {bid}</h1>
     );
